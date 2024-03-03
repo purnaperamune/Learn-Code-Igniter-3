ab6707825a75dccc0e1c64e8e5b74ba87d1628c7
Purna Peramune <purnaperamune@gmail.com>
2024-03-03 19:57:05 +0530

    Simple route with a controller

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,4 +22,10 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+
+	public function demo() {
+		echo "I am who I am";
+		// $this->load->view('welcome_message');
+		// $this -> load -> view('about');
+	}
 }

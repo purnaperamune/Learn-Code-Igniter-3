fd88ce6530a39453be2e707fdd5106dedc7a56b6
Purna Peramune <purnaperamune@gmail.com>
2024-03-03 21:50:16 +0530

    Passing params to models

--- application/controllers/StudentController.php
+++ application/controllers/StudentController.php
@@ -4,14 +4,21 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class StudentController extends CI_Controller{
-    public function indexff(){
+    public function index(){
         $this->load-> model('StudentModel');
 
         // $student = $this -> StudentModel -> studentData();  // Method 1
-        
+
         $student = new StudentModel; // Method 2
         $student = $student -> studentData();
+
         echo "Student Name: ". $student;
     }
+
+    public function show($id){
+        // echo $id;
+        $this -> load -> model('StudentModel', 'stud'); // Giving a short name
+        $selected_stud = $this -> stud -> student_show($id); 
+        echo $selected_stud;
+    }
 }
-?>
--- application/models/StudentModel.php
+++ application/models/StudentModel.php
@@ -2,8 +2,21 @@
 
 class StudentModel extends CI_Model{
     public function studentData(){
-        return $student_name = "Rohit";
+        $student_class = $this -> student_class();
+        return $student_name = "Rohit. <br>His class is " . $student_class;
+    }
+
+    private function student_class(){
+        return $student_class = "BCA";
+    }
+
+    public function student_show($id){
+        if($id == '1'){
+            return $result = "User 1";
+        } 
+        elseif ($id == '2') {
+            return $result = "User 2";
+        }
+        
     }
 }
-
-?>
